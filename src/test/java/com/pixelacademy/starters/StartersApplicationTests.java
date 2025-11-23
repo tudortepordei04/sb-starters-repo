bb229d9ede7ddfb504e55d1905e1fadbd99cc3cf
tudortepordei04 <tepordei.tudor04@gmail.com>
2025-11-23 17:11:06 +0200

    Initial commit

---
+++ src/test/java/com/pixelacademy/starters/StartersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelacademy.starters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
